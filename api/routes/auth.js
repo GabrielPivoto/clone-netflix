e45ef92cfb219ccf92edc13fe19ae233c972ae65
unknown <nathanazevedo@gec.inatel.br>
2022-03-28 22:15:39 -0300

    feat: add  password encrypting when registering data

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,17 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //Register users
 router.post("/register",async (req,res)=>{
     const newUser = new User({
         username:req.body.username,
         email:req.body.email,
-        password:req.body.password,
-    })
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.SECRET_KEY)
+            .toString(),
+    });
     try{
         const user = await newUser.save();
         res.status(201).json(user);
@@ -15,5 +19,15 @@ router.post("/register",async (req,res)=>{
         res.status(500).json(err);
     }
 })
+
+//Login users
+// router.post("login", (req,res)=>{
+//     try{
+//         const user = await newUser.save();
+//         res.status(201).json(user);
+//     }catch (err){
+//         res.status(500).json(err);
+//     }
+// })
 
 module.exports = router;

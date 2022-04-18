6123f78d6e6d5347ccf6c1ffe16f9c7006226bc8
Gabriel Pivoto <gabriel-pivoto@outlook.com>
2022-04-18 18:31:57 -0300

    Adding grayscale feature

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,8 +4,16 @@ import "./navbar.scss"
 
 const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false)
+
+
+    window.onscroll = () =>{
+        setIsScrolled(window.pageYOffset === 0 ? false : true)
+        return () => (window.onscroll = null)
+    }
+
+    console.log(isScrolled)
   return (
-    <div className='navbar'>
+    <div className={isScrolled?"navbar scrolled":"navbar"}>
       <div className="container">
           <div className="left">
               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" 

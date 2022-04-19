ead8db07dbebcb93a52fff2417157d7410987fa6
Gabriel Pivoto <gabriel-pivoto@outlook.com>
2022-04-19 20:26:04 -0300

    List and ListItem fineshed

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -4,28 +4,33 @@ import "./listItem.scss"
 
 export default function ListItem({index}) {
   const [isHovered, setIsHovered] = useState(false)
+  const trailer = "https://media.istockphoto.com/videos/footage-of-rubber-chicken-dark-background-video-id1366799090";
   return (
     <div className='listItem' 
     style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
     onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}>
       <img src="https://cdn.mensagenscomamor.com/content/images/m000532516.jpg?v=2" alt="" />
-      <div className="itemInfo">
-        <div className="icons">
-          <PlayArrow/>
-          <Add/>
-          <ThumbUpAltOutlined/>
-          <ThumbDownAltOutlined/> 
-        </div>
-        <div className="itemInfoTop">
-          <span>1h 32min</span>
-          <span className='limit'>+18</span>
-          <span>2002</span>
-        </div>
-        <div className="description">
-          Tá pegando fogo bixo.
-        </div>
-        <div className="genre">Ação</div>
-      </div>
+      {isHovered && (
+        <>
+        <video src={trailer} autoPlay={true} loop/>
+        <div className="itemInfo">
+          <div className="icons">
+            <PlayArrow className='icon'/>
+            <Add className='icon'/>
+            <ThumbUpAltOutlined className='icon'/>
+            <ThumbDownAltOutlined className='icon'/> 
+          </div>
+          <div className="itemInfoTop">
+            <span>1h 32min</span>
+            <span className='limit'>+18</span>
+            <span>2002</span>
+          </div>
+          <div className="description">
+            Tá pegando fogo bixo.
+          </div>
+          <div className="genre">Ação</div>
+        </div></>
+      )}
     </div>
   )
 }

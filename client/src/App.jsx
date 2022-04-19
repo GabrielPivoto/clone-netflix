cf6619034bce4632cc8e54a8d11efb774502c6c5
Gabriel Pivoto <gabriel-pivoto@outlook.com>
2022-04-19 20:34:14 -0300

    Changing folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./app.scss"
-import Home from "./home/Home";
+import Home from "./pages/home/Home";
 
 const App = () => {
   return <Home/>;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import "./home.scss"
-import Navbar from '../components/navbar/Navbar'
-import Featured from '../components/featured/Featured'
-import List from '../components/list/List'
+import Navbar from '../../components/navbar/Navbar'
+import Featured from "../../components/featured/Featured";
+import "./home.scss";
+import List from "../../components/list/List";
 
 const Home = () => {
   return (

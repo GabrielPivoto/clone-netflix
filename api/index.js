b98c4d02f9ca3a1a2c85648423127bc3ad1ffcd6
unknown <nathanazevedo@gec.inatel.br>
2022-05-11 21:12:53 -0300

    feat: api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ mongoose
 app.use(express.json());
 
 app.use("/api/auth",authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800,()=>{
     console.log("Backend server is running");
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
-//Register users
+//REGISTER USERS
 router.post("/register",async (req,res)=>{
     const newUser = new User({
         username:req.body.username,
@@ -20,14 +21,38 @@ router.post("/register",async (req,res)=>{
     }
 })
 
-//Login users
-// router.post("login", (req,res)=>{
-//     try{
-//         const user = await newUser.save();
-//         res.status(201).json(user);
-//     }catch (err){
-//         res.status(500).json(err);
-//     }
-// })
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        //SEARCH ON DB IF EXISTS GIVEN EMAIL
+      const user = await User.findOne({ email: req.body.email });
+        //IF NOT SEND WARNING
+      !user && res.status(401).json("Wrong password or username!");
+        
+        //IF SO DECRYPT PASSWORD USING SECRET KEY
+      const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        //DECRYPT KEY
+      const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+        
+        //COMPARE WITH THE ONE ON DB
+      originalPassword !== req.body.password &&
+        res.status(401).json("Wrong password or username!"); //IF IT IS BAD SEND WARNING
+        
+        //SIGN IN WITH ACCESS TOKEN
+      const accessToken = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.SECRET_KEY,
+        { expiresIn: "5d" }
+      );
+        
+        //STORES INFO LOCALLY
+      const { password, ...info } = user._doc;
 
-module.exports = router;
+        //RETURN INFO FROM DB + ACESS TOKEN
+      res.status(200).json({ ...info, accessToken });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
